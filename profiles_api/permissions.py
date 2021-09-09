63f861f1bbacdbb30d8fb0e18847a63bfa9fcfc3
echeadle <echeadle@gmail.com>
2021-09-08 18:54:38 -0600

    Add search to user profle

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
 
-    def has_object_permision(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
